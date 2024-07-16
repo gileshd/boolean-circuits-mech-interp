a5dc82ef433cfbfb65d3f0b65a87b44c943da14f
gileshd <gileshd@googlemail.com>
2024-07-16 21:51:19 +0100

    Add multi-task parity data sampling and model training

--- parity_data.py
+++ parity_data.py
@@ -13,18 +13,90 @@ def parity(
     return one_hot(p, 2)
 
 
-def sample_binary_data(
-    key, num_samples: int, data_dim: int
-) -> Float[Array, "data_dim"]:
-    return jnp.array(jr.bernoulli(key, 0.5, (num_samples, data_dim)))
+def sample_binary_data(key, n_samples: int, data_dim: int) -> Float[Array, "data_dim"]:
+    return jr.bernoulli(key, 0.5, (n_samples, data_dim))
 
 
-@partial(jit, static_argnums=(1,2))
+@partial(jit, static_argnums=(1, 2))
 def sample_binary_parity_data(
-    key, num_samples: int, dim: int, idx_mask: Bool[Array, "data_dim"]
-) -> tuple[Bool[Array, "num_samples data_dim"], Float[Array, "num_samples 2"]]:
+    key, n_samples: int, dim: int, idx_mask: Bool[Array, "data_dim"]
+) -> tuple[Bool[Array, "n_samples data_dim"], Float[Array, "n_samples 2"]]:
     """docstring"""
-    x = sample_binary_data(key, num_samples, dim)
+    x = sample_binary_data(key, n_samples, dim)
     y = vmap(parity, in_axes=(0, None))(x, idx_mask)
     return x, y
 
+
+def indices_to_mask(indices: Array, data_dim) -> Bool[Array, "data_dim"]:
+    return jnp.zeros(data_dim, dtype=bool).at[indices].set(True)
+
+
+def make_task_bit_mask_array(
+    key, n_tasks: int, n_bits_per_task: int, data_dim: int
+) -> Bool[Array, "n_tasks data_dim"]:
+    """
+    Creates an array of task bit masks.
+
+    Args:
+        key: The random key used for generating task bit masks.
+        n_tasks (int): The number of tasks.
+        n_bits_per_task (int): The number of bits per task.
+        data_dim (int): The dimension of the data.
+
+    Returns:
+        jnp.ndarray: An array where the first dimension corresponds to task indices and the second dimension to the data dimension, with True values indicating task bits.
+    """
+    assert n_tasks * n_bits_per_task <= data_dim
+    task_bit_mask_array = jnp.zeros((n_tasks, data_dim), dtype=bool)
+    data_indices = list(range(data_dim))
+    for i in range(n_tasks):
+        key, subkey = jr.split(key)
+        task_bits = jr.choice(
+            subkey, jnp.array(data_indices), (n_bits_per_task,), replace=False
+        )
+        task_mask = jnp.zeros(data_dim, dtype=bool).at[task_bits].set(True)
+        task_bit_mask_array = task_bit_mask_array.at[i].set(task_mask)
+        for idx in task_bits:
+            data_indices.remove(idx)
+    return task_bit_mask_array
+
+
+def sample_task_bits(key, n_samples, n_tasks):
+    # TODO: allow for specifying prob of each task: jr.choice(..., p=...)
+    tasks = jr.choice(key, jnp.arange(n_tasks), (n_samples,))
+    return one_hot(tasks, n_tasks)
+
+
+def sample_multitask_parity_data(
+    key, n_samples: int, n_tasks: int, n_bits_per_task: int, data_dim: int
+) -> tuple[
+    Bool[Array, "n_samples n_tasks+data_dim"],
+    Float[Array, "n_samples 2"],
+    Bool[Array, "n_tasks data_dim"],
+]:
+    """
+    Sample multi-task binary parity data.
+
+    Args:
+        key: The random key used for generating task bit masks.
+        n_samples (int): The number of samples.
+        dim (int): The dimension of the data.
+        n_tasks (int): The number of tasks.
+        n_bits_per_task (int): The number of bits per task.
+
+    Returns:
+        tuple[Bool[Array, "n_samples data_dim"], Float[Array, "n_samples n_tasks 2"]]: A tuple containing the input data and the output labels.
+    """
+    task_bit_mask_array = make_task_bit_mask_array(
+        key, n_tasks, n_bits_per_task, data_dim
+    )
+
+    key1, key2 = jr.split(key)
+    task_bits = sample_task_bits(key1, n_samples, n_tasks)
+    data_bits = sample_binary_data(key2, n_samples, data_dim)
+
+    _task_parity = lambda task_idx, data: parity(data, task_bit_mask_array[task_idx])
+    y = vmap(_task_parity, in_axes=(0, 0))(task_bits.argmax(1), data_bits)
+
+    x = jnp.concatenate((task_bits, data_bits), axis=1)
+    return x, y, task_bit_mask_array
---
+++ train_multitask_sparse_parity.py
@@ -0,0 +1,78 @@
+from jax import numpy as jnp
+from jax import random as jr
+from jax import grad, jit
+from jax.nn import log_softmax
+import optax
+
+from model import MLP
+from parity_data import sample_multitask_parity_data
+from utils import create_minibatches
+
+## Model ##
+data_bits = 16
+n_tasks = 3
+data_dim = data_bits + n_tasks
+
+model = MLP(features=[32, 32, 2])
+key = jr.PRNGKey(0)
+params = model.init(key, jnp.ones(data_dim))
+
+## Data ##
+N = 5000
+key, subkey = jr.split(key)
+n_bits_per_task = 3
+x, y, _ = sample_multitask_parity_data(subkey, N, n_tasks, n_bits_per_task, data_bits)
+
+train_N = int(0.8 * N)
+x_train, y_train = x[:train_N], y[:train_N]
+x_test, y_test = x[train_N:], y[train_N:]
+
+## Training Setup ##
+def loss_fn(params, x, y):
+    """Cross entropy loss."""
+    logits = model.apply(params, x)
+    log_probs = log_softmax(logits)
+    return -jnp.sum(y * log_probs) / y.shape[0]
+
+
+optimizer = optax.adam(learning_rate=0.01)
+opt_state = optimizer.init(params)
+
+@jit
+def update(params, x, y, opt_state):
+    grads = grad(loss_fn)(params, x, y)
+    updates, opt_state = optimizer.update(grads, opt_state)
+    new_params = optax.apply_updates(params, updates)
+    return new_params, opt_state
+
+
+## Training Loop ##
+def accuracy(params, x, y):
+    """Calculate the accuracy of `model` and `params` on a given dataset."""
+    logits: Array = model.apply(params, x)  # type: ignore
+    return sum(jnp.argmax(logits, axis=1) == jnp.argmax(y, axis=1)) / y.shape[0]
+
+
+train_loss = []
+test_loss = []
+
+# Training loop
+batch_size = 128
+num_epochs = 100
+key = jr.PRNGKey(0)
+for epoch in range(num_epochs):
+    key, subkey = jr.split(key)
+
+    for x_batch, y_batch in create_minibatches(x_train, y_train, batch_size, subkey):
+        params, opt_state = update(params, x_batch, y_batch, opt_state)
+
+    if epoch % 10 == 0:
+        current_train_loss = loss_fn(params, x_train, y_train)
+        current_test_loss = loss_fn(params, x_test, y_test)
+        train_loss.append(current_train_loss)
+        test_loss.append(current_test_loss)
+        print(f"Epoch {epoch}")
+        print(f"\tTrain Loss: {current_train_loss:.4e}")
+        print(f"\tTest Loss: {current_test_loss:.4e}")
+        print(f"\tTrain Accuracy: {accuracy(params, x_train, y_train):.3f}")
+        print(f"\tTest Accuracy: {accuracy(params, x_test, y_test):.3f}")
