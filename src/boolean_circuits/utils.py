f5d31e9d3e0a1e9b79ab1f194ba2de8fa8476c92
gileshd <gileshd@googlemail.com>
2024-08-16 20:52:05 +0100

    Add script for simple XOR probing example

--- src/boolean_circuits/utils.py
+++ src/boolean_circuits/utils.py
@@ -1,9 +1,12 @@
 import itertools
+from typing import Iterable, Generator, Tuple, Any
 
 
-def all_combinations(iterable, min_length=0, max_length=None):
+def all_combinations(
+    iterable: Iterable[Any], min_length: int = 0, max_length: int | None = None
+) -> Generator[Tuple[Any, ...], None, None]:
     """Generate all combinations of all lengths for the given iterable."""
-    max_length = max_length if max_length is not None else len(iterable)
+    max_length = max_length if max_length is not None else len(tuple(iterable))
     for r in range(min_length, max_length + 1):
         for combination in itertools.combinations(iterable, r):
             yield combination
