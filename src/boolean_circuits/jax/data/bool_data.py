53b57d9ea3e9a2363b6c3dbe7c443f1cf47b27d7
gileshd <gileshd@googlemail.com>
2024-08-17 10:58:56 +0100

    Add function to generate all binary arrays of a length

--- src/boolean_circuits/jax/data/bool_data.py
+++ src/boolean_circuits/jax/data/bool_data.py
@@ -1,37 +1,41 @@
+import itertools
 from jax import vmap
 from jax import numpy as jnp
 from jax import random as jr
+from jaxtyping import Array, Int
 
 
 def n_to_binary_array(number, num_bits=8):
     """
     Convert a number to its binary representation as a JAX array.
-    
+
     Args:
     number (int): The number to convert.
     num_bits (int): The number of bits to use for the representation.
-    
+
     Returns:
     jax.Array: A JAX array containing the binary representation.
     """
-    binary = jnp.arange(num_bits - 1,-1,-1)
+    binary = jnp.arange(num_bits - 1, -1, -1)
     number = jnp.where(number < 2**num_bits, number, jnp.nan)
-    return (number // (2 ** binary)) % 2
+    return (number // (2**binary)) % 2
+
 
 def binary_array_to_number(binary_array):
     """
     Convert a binary representation (JAX array) back to its decimal number.
     The input array should have the least significant bit as the rightmost element.
-    
+
     Args:
     binary_array (jax.Array): The binary representation as a JAX array.
-    
+
     Returns:
     int: The decimal representation of the binary array.
     """
     num_bits = binary_array.shape[0]
     powers = jnp.arange(num_bits - 1, -1, -1)
-    return jnp.sum(binary_array * (2 ** powers))
+    return jnp.sum(binary_array * (2**powers))
+
 
 def generate_bin_addition_data(key, n_samples=100):
     """
@@ -46,6 +50,15 @@ def generate_bin_addition_data(key, n_samples=100):
 
     a_bin = vmap(n_to_binary_array)(a)
     b_bin = vmap(n_to_binary_array)(b)
-    x = jnp.concatenate([a_bin, b_bin], axis=1) 
+    x = jnp.concatenate([a_bin, b_bin], axis=1)
     y = vmap(lambda n: n_to_binary_array(n, num_bits=9))(c)
-    return x,y
+    return x, y
+
+
+def generate_complete_bin_data(key, dim: int) -> Int[Array, "n_combs {dim}"]:
+    """
+    Generate all combinations of arrays of binary data length `dim`, return shuffled.
+    """
+    x = jnp.array(list(itertools.product([0, 1], repeat=dim)))
+    return jr.permutation(key, x, axis=0)
+
