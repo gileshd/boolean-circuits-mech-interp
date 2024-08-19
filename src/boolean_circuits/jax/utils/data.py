71ce9dc2a8922a3e2b615905d2ea7a4de8b20f8a
gileshd <gileshd@googlemail.com>
2024-08-19 12:02:23 +0100

    Add sorting function for boolean arrays

--- src/boolean_circuits/jax/utils/data.py
+++ src/boolean_circuits/jax/utils/data.py
@@ -1,6 +1,8 @@
 from jax import numpy as jnp
 from jax import random as jr
 from jax.tree_util import tree_map
+from jaxtyping import Array, Int
+
 
 def create_minibatches(data, batch_size, rng_key):
     """
@@ -19,7 +21,9 @@ def create_minibatches(data, batch_size, rng_key):
     generator
         A generator that yields PyTrees batched data.
     """
-    assert len(set(tree_map(lambda a: a.shape[0], data))) == 1, "All arrays must have the same first dimension"
+    assert (
+        len(set(tree_map(lambda a: a.shape[0], data))) == 1
+    ), "All arrays must have the same first dimension"
     num_examples = data[0].shape[0]
 
     # Shuffle data and labels in unison
@@ -35,3 +39,21 @@ def create_minibatches(data, batch_size, rng_key):
         end_idx = start_idx + batch_size
         batch = tree_map(lambda a: a[start_idx:end_idx], shuffled_data)
         yield batch
+
+
+# TODO: The type hints here should really be `Union[Bool, Int]` but 
+#        I'm not sure how to get jaxtyping to play nice with this this...
+def sort_binary_array(
+    x: Int[Array, "n_samples n_bits"]
+) -> tuple[Int[Array, "n_samples n_bits"], Int[Array, "n_samples"]]:
+    """Sort rows of binary array according to their integer value.
+
+    Args:
+        x: Binary array of shape (n_samples, n_bits) where each row is a binary number.
+
+    Returns:
+        Tuple: Sorted binary array and the indices that were used to sort the array
+    """
+
+    sorted_idx = jnp.argsort(jnp.packbits(x, axis=1).flatten())
+    return x[sorted_idx], sorted_idx
